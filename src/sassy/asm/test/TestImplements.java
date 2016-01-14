b3d02327d7a5867245357ea7f0c6be449bff3310
theoderic <dsfreak17@gmail.com>
2016-01-14 14:47:09 -0500

    Added JUnit test files

---
+++ src/sassy/asm/test/TestImplements.java
@@ -0,0 +1,14 @@
+package sassy.asm.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestImplements {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/sassy/asm/test/TestInheritance.java
@@ -0,0 +1,14 @@
+package sassy.asm.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestInheritance {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
